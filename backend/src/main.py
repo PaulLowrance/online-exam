46cc149af6b23f3d8f12370bc2f439f4d594e11b
Unknown <tpaul.lowrance@gmail.com>
2018-06-24 18:06:47 -0500

    Integrated Flask RESTful endpoints and SQLAlchemy
    adds get & post for exams

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,28 +1,47 @@
 # coding=utf-8
 
+from flask import Flask, jsonify, request
+
 from .entities.entity import Session, engine, Base
-from .entities.exam import Exam
+from .entities.exam import Exam, ExamSchema
+
+
+# create the flask application
+app = Flask(__name__)
 
 # generate database schema
 Base.metadata.create_all(engine)
 
-# start session
-session = Session()
 
-# check for existing data
-exams = session.query(Exam).all()
+@app.route('/exams')
+def get_exams():
+    # fetch from the database
+    session = Session()
+    exam_objects = session.query(Exam).all()
 
-if len(exams) == 0:
-    # create and persist dummy exam
-    python_exam = Exam("SQLAlchemy Exam", "Test your knowledge about SQLAlchemy.", "script")
-    session.add(python_exam)
+    # transforming into JSON serializable objects
+    schema = ExamSchema(many=True)
+    exam = schema.dump(exam_objects)
+
+    session.close()
+    return jsonify(exam.data)
+
+
+@app.route('/exams', methods=['POST'])
+def add_exam():
+    # mount an exam object
+    posted_exam = ExamSchema(only=('title', 'description')).load(request.get_json())
+
+    exam = Exam(**posted_exam.data, created_by="HTTP Post request")
+
+    # persist exam
+    session = Session()
+    session.add(exam)
     session.commit()
+
+    new_exam = ExamSchema().dump(exam).data
+
     session.close()
 
-    # reload exams
-    exams = session.query(Exam).all()
+    return jsonify(new_exam), 201
 
-# show existing exams
-print('### Exams:')
-for exam in exams:
-    print(f'({exam.id}) {exam.title} - {exam.description}')
